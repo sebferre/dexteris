4bf0938420e1793dec84634f3fceaad27844d881
Sebastien Ferre <ferre@irisa.fr>
2020-11-12 23:14:25 +0100

    [rdf] more complete definition of 'printTurtle'

--- rdf.ml
+++ rdf.ml
@@ -11,88 +11,115 @@ let escape l s =
 let escape_uri = escape [(">","\\>")]
 let escape_shortString = escape [("\"","\\\""); ("\n","\\n"); ("\r","\\r"); ("\t","\\t")]
 let escape_longString = escape [("\"","\\\"")]
-  
-       
-let rec obj_of_atom (i : item) : (string * item list) option =
-  match i with
-  | `Bool b -> Some ((if b then "true" else "false"), [])
-  | `Int i -> Some (string_of_int i, [])
-  | `Float f -> Some (string_of_float f, [])
-  | `String s -> Some ("\"" ^ escape_shortString s ^ "\"", [])
-  | `Null -> None
-  | `Assoc ["@value", `String v; "@language", `String l] -> Some ("\"" ^ escape_shortString v ^ "\"@" ^ l, [])
-  | `Assoc ["@value", `String v; "@type", `String t] -> Some ("\"" ^ escape_shortString v ^ "\"^^" ^ t, [])
-  | `Assoc ["@list", `List li] ->
-     let lo,ln =
-       List.fold_right
-	 (fun i (lo,ln) ->
-	  match obj_of_atom i with
-	  | Some (o, ln1) -> o::lo, ln1@ln
-	  | None -> lo, ln)
-	 li ([],[]) in
-     Some ("(" ^ String.concat " " lo ^ ")", ln)
-  | `Assoc pairs ->
-     (match List.assoc_opt "@id" pairs with
-      | Some (`String s) -> Some (s, [i])
-      | _ -> failwith "turtle_of_data: invalid_object")
-  | `List _ -> failwith "turtle_of_data: invalid object (array)"
 
 let rec turtle_jsonld buf d =
   d |> Seq.iter (turtle_node buf)
 and turtle_node buf = function
+  | `Assoc pairs
+       when List.exists (fun (k,i) -> k<>"@id") pairs ->
+       (* else no triple *)
+     let () =
+       match List.assoc_opt "@id" pairs with
+       | Some (`String s) -> Buffer.add_string buf s
+       | Some _ -> failwith "turtle_of_data: invalid @id"
+       | None -> Buffer.add_string buf "[]" in
+     let ln = turtle_props buf "\n    " [] pairs in
+     Buffer.add_string buf " .\n";
+     List.iter (turtle_node buf) (List.rev ln)
+  | _ -> failwith "turtle_of_data: invalid node description"
+and turtle_props buf indent ln pairs =
+  let not_first = ref false in
+  List.fold_left
+    (fun ln (k,i) ->
+     match k with
+     | "@id" -> ln
+     | "@type" ->
+	let lt =
+	  match i with
+	  | `String t -> [t]
+	  | `List li ->
+	     List.fold_right
+	       (fun i res ->
+		match i with
+		| `String t -> t::res
+		| _ -> failwith "turtle_of_data: unexpected type")
+	       li []
+	  | _ -> failwith "turtle_of_data: unexpected type" in
+	if lt = [] then failwith "turtle_of_data: unexpected type";
+	if !not_first then Buffer.add_string buf " ;";
+	not_first := true;
+	Buffer.add_string buf indent;
+	Buffer.add_string buf "a ";
+	Buffer.add_string buf (String.concat ", " lt);
+	ln
+     | p ->
+	if !not_first then Buffer.add_string buf " ;";
+	not_first := true;
+	Buffer.add_string buf indent;
+	Buffer.add_string buf p;
+	Buffer.add_string buf " ";
+	turtle_objs buf (indent ^ "    ") ln i)
+    ln pairs
+and turtle_objs buf indent ln i =
+  match i with
+  | `List li ->
+     let not_first = ref false in
+     List.fold_left
+       (fun ln i ->
+	if !not_first then Buffer.add_string buf ", ";
+	let ok, ln = turtle_obj buf indent ln i in
+	not_first := !not_first || ok;
+	ln)
+       ln li
+  | _ ->
+     let ok, ln = turtle_obj buf indent ln i in
+     ln
+and turtle_obj buf indent ln i =
+  match i with
+  | `Bool b ->
+     Buffer.add_string buf (if b then "true" else "false");
+     true, ln
+  | `Int i ->
+     Buffer.add_string buf (string_of_int i);
+     true, ln
+  | `Float f ->
+     Buffer.add_string buf (string_of_float f);
+     true, ln
+  | `String s ->
+     Buffer.add_string buf ("\"" ^ escape_shortString s ^ "\"");
+     true, ln
+  | `Null -> false, ln
+  | `Assoc ["@value", `String v; "@language", `String l] ->
+     Buffer.add_string buf ("\"" ^ escape_shortString v ^ "\"@" ^ l);
+     true, ln
+  | `Assoc ["@value", `String v; "@type", `String t] ->
+     Buffer.add_string buf ("\"" ^ escape_shortString v ^ "\"^^" ^ t);
+     true, ln
+  | `Assoc ["@list", `List li] ->
+     Buffer.add_string buf "(";
+     let not_first = ref false in
+     let ln =
+       List.fold_left
+	 (fun ln elt ->
+	  if !not_first then Buffer.add_string buf " ";
+	  let ok, ln = turtle_obj buf indent ln elt in
+	  not_first := !not_first || ok;
+	  ln)
+	 ln li in
+     Buffer.add_string buf ")";
+     true, ln
   | `Assoc pairs ->
-     let s, lt, lpo, ln = (* subject IRI, types, properties, sub-nodes *)
-       List.fold_right
-	 (fun (k,i) (s,lt,lpo,ln) ->
-	  match k, i with
-	  | "@id", `String s -> s, lt, lpo, ln
-	  | "@id", _ -> failwith "turtle_of_data: invalid @id"
-	  | "@type", `String t -> s, t::lt, lpo, ln
-	  | "@type", `List li ->
-	     let lt =
-	       List.fold_right
-		 (fun i res ->
-		  match i with
-		  | `String t -> t::res
-		  | _ -> res)
-		 li lt in
-	     s, lt, lpo, ln
-	  | "@type", _ -> failwith "turtle_of_data: invalid @type"
-	  | _, `List li ->
-	     let lo, ln =
-	       List.fold_right
-		 (fun i (lo,ln) ->
-		  match obj_of_atom i with
-		  | Some (o, ln1) -> o::lo, ln1@ln
-		  | None -> lo, ln)
-		 li ([], ln) in
-	     s, lt, (k,lo)::lpo, ln
-	  | _, _ ->
-	     (match obj_of_atom i with
-	      | Some (o, ln1) -> s, lt, (k,[o])::lpo, ln1@ln
-	      | None -> s, lt, lpo, ln))
-	 pairs ("[]", [], [], []) in
-     if lt <> [] || lpo <> [] then (
-       Buffer.add_string buf s;
-       if lt <> [] then (
-	 Buffer.add_string buf " a ";
-	 Buffer.add_string buf (String.concat ", " lt)
-       );
-       let first = ref true in
-       List.iter
-	 (fun (p,lo) ->
-	  if not !first || lt <> [] then Buffer.add_string buf ";";
-	  first := false;
-	  Buffer.add_string buf "\n    ";
-	  Buffer.add_string buf p;
-	  Buffer.add_string buf " ";
-	  Buffer.add_string buf (String.concat ", " lo))
-	 lpo;
-       Buffer.add_string buf ".\n"
-     );
-     List.iter (turtle_node buf) ln
-  | _ -> ()
-
+     (match List.assoc_opt "@id" pairs with
+      | Some (`String s) ->
+	 Buffer.add_string buf s;
+	 true, i::ln
+      | Some _ -> failwith "turtle_of_data: invalid_object"
+      | None ->
+	 Buffer.add_string buf "[";
+	 let ln = turtle_props buf indent ln pairs in
+	 Buffer.add_string buf " ]";
+	 true, ln)
+  | `List _ -> failwith "turtle_of_data: invalid object (array)"
        
 let turtle_of_data (d : data) : string =
   let buf = Buffer.create 10103 in
